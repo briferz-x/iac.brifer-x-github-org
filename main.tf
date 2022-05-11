d1c99dfa11821c609b14d42315e079839507aa05
Luis Brito <luis.brfernandez@gmail.com>
2022-05-11 17:28:56 -0500

    Add github owner

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
-provider "github" {}
+provider "github" {
+  owner = var.github_owner
+}
 
 data "github_organization" "organization" {
   name = "briferz-x"
+}
+
+resource "github_repository" "this-repo" {
+  name = "iac.brifer-x-github-org"
+  visibility = "private"
 }
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "github_owner" {
+  type = string
+  description = "The GitHub organization or user the template repository is owned by."
+}
