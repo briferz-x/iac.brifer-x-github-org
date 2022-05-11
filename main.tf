d3ce0fd4b4ef022233df7e8815c54c1f4a33a7bf
Luis Brito <luis.brfernandez@gmail.com>
2022-05-11 17:57:53 -0500

    Add this repository fully

--- main.tf
+++ main.tf
@@ -3,10 +3,26 @@ provider "github" {
 }
 
 data "github_organization" "organization" {
-  name = "briferz-x"
+  name = var.github_owner
 }
 
 resource "github_repository" "this-repo" {
   name = "iac.brifer-x-github-org"
-  visibility = "private"
+
+  visibility           = "private"
+  has_downloads        = true
+  has_issues           = true
+  has_projects         = true
+  has_wiki             = true
+  vulnerability_alerts = true
+}
+
+resource "github_branch" "this-repo-main-branch" {
+  branch     = "main"
+  repository = github_repository.this-repo.id
+}
+
+resource "github_branch_default" "this-repo-branch-default" {
+  repository = github_repository.this-repo.id
+  branch     = github_branch.this-repo-main-branch.branch
 }
