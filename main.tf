b38334b5ac6c1db5bbbfe729e5d19270fd03a3e7
Luis Brito <luis@MacBook-Pro-de-Luis.local>
2022-05-16 11:01:04 -0500

    Apply team_membership module into membership module

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ module "membership" {
   for_each = local.member_mapping
 
   member_conf = each.value
-  teams       = module.team
+  teams       = {for team_name, team_module in module.team : team_name => team_module.team}
 }
 
 locals {
--- modules/membership/main.tf
+++ modules/membership/main.tf
@@ -1,9 +1,23 @@
 locals {
-  is_owner        = lookup(var.member_conf, "is_owner", false)
-  membership_role = local.is_owner? "admin" : "member"
+  is_owner            = lookup(var.member_conf, "is_owner", false)
+  membership_role     = local.is_owner? "admin" : "member"
+  member_teams        = lookup(var.member_conf, "teams", [])
+  member_team_mapping = {
+  for team in local.member_teams : team["name"] => team
+  }
 }
 
 resource "github_membership" "membership" {
   username = var.member_conf["username"]
   role     = local.membership_role
+}
+
+module "team_membership" {
+  source = "../team_membership"
+
+  for_each = local.member_team_mapping
+
+  membership       = github_membership.membership
+  team             = var.teams[each.key]
+  team_member_conf = each.value
 }
