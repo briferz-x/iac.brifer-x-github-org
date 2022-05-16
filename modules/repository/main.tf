5ecd3a82286dafcb73df3f4edeb5789eef6358eb
Luis Brito <luis@MacBook-Pro-de-Luis.local>
2022-05-16 12:25:07 -0500

    Add default attributes to repo

--- modules/repository/main.tf
+++ modules/repository/main.tf
@@ -5,12 +5,17 @@ locals {
 resource "github_repository" "repository" {
   name = var.repo_conf["name"]
 
-  visibility           = local.visibility
-  has_downloads        = lookup(var.repo_conf, "has_downloads", true)
-  has_issues           = lookup(var.repo_conf, "has_issues", true)
-  has_projects         = lookup(var.repo_conf, "has_projects", true)
-  has_wiki             = lookup(var.repo_conf, "has_wiki", true)
-  vulnerability_alerts = lookup(var.repo_conf, "vulnerability_alerts", true)
+  visibility             = local.visibility
+  delete_branch_on_merge = lookup(var.repo_conf, "delete_branch_on_merge", true)
+  allow_squash_merge     = lookup(var.repo_conf, "allow_squash_merge", true)
+  allow_merge_commit     = lookup(var.repo_conf, "allow_merge_commit", true)
+  allow_rebase_merge     = lookup(var.repo_conf, "allow_rebase_merge", true)
+  has_downloads          = lookup(var.repo_conf, "has_downloads", true)
+  has_issues             = lookup(var.repo_conf, "has_issues", true)
+  has_projects           = lookup(var.repo_conf, "has_projects", true)
+  has_wiki               = lookup(var.repo_conf, "has_wiki", true)
+  is_template            = lookup(var.repo_conf, "is_template", false)
+  vulnerability_alerts   = lookup(var.repo_conf, "vulnerability_alerts", true)
 }
 
 locals {
